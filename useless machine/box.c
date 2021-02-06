74d8501fe606984b90487a1c5fc52fd2d47a9d2e
Hariharan <cvhariharan@protonmail.com>
2021-02-06 08:52:26 +0530

    Add useless machine

--- useless machine/box.c
+++ useless machine/box.c
@@ -5,21 +5,27 @@
 
 void main() {
     const int SERVO_PIN = 4;
+    const int SWITCH_PIN = 6;
 
     Servo *servo = servo_init(SERVO_PIN);
+
+    gpio_init(SWITCH_PIN);
+    gpio_set_dir(SWITCH_PIN, false);
+    gpio_pull_up(SWITCH_PIN);
+
+    servo_move_to_angle(servo, 0);
+    sleep_ms(500);
+
     while(1) {
-        servo_move_to_angle(servo, 90);
+        sleep_ms(500);
+        int val = gpio_get(SWITCH_PIN);
+        if(val) {
+            continue;
+        }
+        servo_move_to_angle(servo, 70);
         sleep_ms(1000);
-        servo_move_to_angle(servo, 180);
-        sleep_ms(1000);
-        servo_move_to_angle(servo, 30);
-        sleep_ms(1000);
-        servo_move_to_angle(servo, 50);
-        sleep_ms(1000);
-        servo_move_to_angle(servo, 10);
-        sleep_ms(1000);
-        servo_move_to_angle(servo, 110);
-        sleep_ms(1000);
+        servo_move_to_angle(servo, 0);
+        sleep_ms(200);
     }
 }
 
